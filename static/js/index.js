5fdd0953d5123c73d51ecdb0280ed5a0da45fea8
zivmax <zivmax@foxmail.com>
2024-08-10 17:15:14 +0800

    feat: Update the interpolation video

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,12 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
 var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
+var NUM_INTERP_FRAMES = 82;
 
 var interp_images = [];
 function preloadInterpolationImages() {
   for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + "/" + String(i).padStart(6, "0") + ".jpg";
+    var path = INTERP_BASE + "/" + String(i+1).padStart(6, "0") + ".jpg";
     interp_images[i] = new Image();
     interp_images[i].src = path;
   }
