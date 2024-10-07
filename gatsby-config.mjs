ab6c98cbd92b484c8d2395d07bcdf8599944d232
zivmax <zivmax@foxmail.com>
2024-10-07 13:58:42 +0800

    chore: update config.

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -16,12 +16,6 @@ const config = {
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-sitemap",
-    // {
-    //   resolve: "gatsby-plugin-manifest",
-    //   options: {
-    //     icon: "src/images/icon.png",
-    //   },
-    // },
     "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
